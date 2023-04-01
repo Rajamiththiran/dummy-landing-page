c9cead66a87b950644fdd9a7fa81c2e291c7cdad
Rajamiththiran <rjmithu7@gmail.com>
2023-04-01 17:47:38 +0530

    Declared some Variables for NavigationBar

--- src/Components/Navigationbar.js
+++ src/Components/Navigationbar.js
@@ -1,12 +1,51 @@
-import React from 'react';
+import CommentRoundedIcon from "@mui/icons-material/CommentRounded";
+import HomeIcon from "@mui/icons-material/Home";
+import InfoIcon from "@mui/icons-material/Info";
+import PhoneRoundedIcon from "@mui/icons-material/PhoneRounded";
+import ShoppingCartRoundedIcon from "@mui/icons-material/ShoppingCartRounded";
+import {
+  Box,
+  Drawer,
+  ListItem,
+  ListItemButton,
+  ListItemIcon,
+  ListItemText,
+} from "@mui/material";
+import React, { useState } from 'react';
 import { BsCart2 } from "react-icons/bs";
 import { HighOutlineBars3 } from "react-icons/hi2";
-import Logo from './Assets/VRM PHARM.svg';
+// import Logo from './Assets/VRM PHARM.svg';
 
 function Navigationbar() {
+
+  const [openMenu,setOpenMenu] = useState(false)
+  const menuOptions = [
+    {
+      text:"Home",
+      icon:<HomeIcon/>,
+    },
+    {
+      text:"About",
+      icon:<InfoIcon/>,
+    },
+    {
+      text:"Services",
+      icon:<CommentRoundedIcon/>,
+    },
+    {
+      text:"Contact",
+      icon:<PhoneRoundedIcon/>,
+    },
+    {
+      text:"Cart",
+      icon:<ShoppingCartRoundedIcon/>,
+    },
+  ]
+
+
   return (
     <div>Navigationbar</div>
   )
 }
 
-export default Navigationbar
+export default Navigationbar 
